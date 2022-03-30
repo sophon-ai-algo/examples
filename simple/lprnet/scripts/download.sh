e43ed68a642e7d28f49378ab160f490838376b26
zhongpan.chen <zhongpan.chen@sophgo.com>
2022-03-30 10:10:38 +0800

    fix(lprnet):fix download IP bugs
    Change-Id: Ib175cf22291d95bffb95906f48179673193f8799

--- simple/lprnet/scripts/download.sh
+++ simple/lprnet/scripts/download.sh
@@ -7,23 +7,24 @@ echo $scripts_dir
 
 pushd $scripts_dir
 # test
-./download_from_nas.sh https://disk.sophgo.vip/sharing/4EVs06DoX
+./download_from_nas.sh http://219.142.246.77:65000/sharing/4EVs06DoX
 tar -xvf test.tar -C ../data/images/
 rm test.tar
 # test_md5
-./download_from_nas.sh https://disk.sophgo.vip/sharing/l9OfrBf6y
+./download_from_nas.sh http://219.142.246.77:65000/sharing/l9OfrBf6y
 tar -xvf test_md5.tar -C ../data/images/
 rm test_md5.tar
 # Final_LPRNet_model.pth
-./download_from_nas.sh https://disk.sophgo.vip/sharing/2GPOnD1pj
+mkdir ../data/models
+./download_from_nas.sh http://219.142.246.77:65000/sharing/2GPOnD1pj
 mv Final_LPRNet_model.pth ../data/models
 # LPRNet_model.torchscript
-./download_from_nas.sh https://disk.sophgo.vip/sharing/xe2BiHGkV
+./download_from_nas.sh http://219.142.246.77:65000/sharing/xe2BiHGkV
 mv LPRNet_model.torchscript ../data/models
 # lprnet_fp32_1b.bmodel
-./download_from_nas.sh https://disk.sophgo.vip/sharing/h4NxSzjHy
+./download_from_nas.sh http://219.142.246.77:65000/sharing/h4NxSzjHy
 mv lprnet_fp32_1b.bmodel ../data/models
 # lprnet_int8_1b.bmodel
-./download_from_nas.sh https://disk.sophgo.vip/sharing/AIRJCf2BS
+./download_from_nas.sh http://219.142.246.77:65000/sharing/AIRJCf2BS
 mv lprnet_int8_1b.bmodel ../data/models
 popd
