19431f0de434c13b2b2d3896d32f98c0275b918d
chenxi.liu <chenxi.liu@sophgo.com>
2022-05-10 17:05:41 +0800

    fix(yolov5):fix scripts when int8 quantization
    Change-Id: Icc4d5d680801a371d7f94f760667fff2cc27c059
    (cherry picked from commit 167a4b9863e6689e271dd46286c32f9dc1c67680)

--- simple/yolov5/scripts/2_gen_int8bmodel.sh
+++ simple/yolov5/scripts/2_gen_int8bmodel.sh
@@ -5,7 +5,7 @@ source model_info.sh
 check_file $src_model_file
 check_dir $lmdb_src_dir
 
-./20_create_lmdb_demo.sh
+./20_create_lmdb.sh
 ./21_gen_fp32umodel.sh
 ./22_modify_fp32umodel.sh
 ./23_gen_int8umodel.sh
